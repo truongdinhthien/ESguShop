aecc396253485c2480a5d94c63f81083b9ccabac
Trương Đình Thiện <truongdinhthien260599@gmail.com>
2020-12-18 23:27:14 +0700

    [Fix] Order

--- WebMVCUI/Service/CurrentBuyerService.cs
+++ WebMVCUI/Service/CurrentBuyerService.cs
@@ -37,7 +37,7 @@ namespace WebMVCUI.Service
                 Address = customer.Address,
                 Email = user.Email,
                 PhoneNumber = user.PhoneNumber,
-                Orders = customer.Orders,
+                Orders = customer.Orders ?? null,
             };
         }
     }
